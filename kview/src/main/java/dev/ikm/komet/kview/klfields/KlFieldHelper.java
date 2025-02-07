ca5d5ad6fdcbcc79b97deb88bb7a426a6b7a8625
bhharsh13 <bhavani.sreehari@gmail.com>
2025-02-07 10:38:13 -0700

    Edit one semantic field value changes

--- kview/src/main/java/dev/ikm/komet/kview/klfields/KlFieldHelper.java
+++ kview/src/main/java/dev/ikm/komet/kview/klfields/KlFieldHelper.java
@@ -1,8 +1,16 @@
 package dev.ikm.komet.kview.klfields;
 
+import static dev.ikm.komet.kview.events.genediting.PropertyPanelEvent.OPEN_PANEL;
+import static dev.ikm.komet.kview.events.genediting.PropertyPanelEvent.SHOW_EDIT_SEMANTIC_FIELDS;
 import static dev.ikm.komet.kview.mvvm.model.DataModelHelper.obtainObservableField;
+import static dev.ikm.komet.kview.mvvm.viewmodel.GenEditingViewModel.SEMANTIC;
+import static dev.ikm.komet.kview.mvvm.viewmodel.GenEditingViewModel.WINDOW_TOPIC;
+
+import dev.ikm.komet.framework.events.EvtBusFactory;
 import dev.ikm.komet.framework.observable.ObservableField;
 import dev.ikm.komet.framework.view.ViewProperties;
+import dev.ikm.komet.kview.controls.KLReadOnlyDataTypeControl;
+import dev.ikm.komet.kview.events.genediting.PropertyPanelEvent;
 import dev.ikm.komet.kview.klfields.booleanfield.KlBooleanFieldFactory;
 import dev.ikm.komet.kview.klfields.componentfield.KlComponentFieldFactory;
 import dev.ikm.komet.kview.klfields.componentfield.KlComponentSetFieldFactory;
@@ -12,6 +20,7 @@ import dev.ikm.komet.kview.klfields.integerfield.KlIntegerFieldFactory;
 import dev.ikm.komet.kview.klfields.readonly.ReadOnlyKLFieldFactory;
 import dev.ikm.komet.kview.klfields.stringfield.KlStringFieldFactory;
 import dev.ikm.komet.kview.mvvm.model.DataModelHelper;
+import dev.ikm.komet.kview.mvvm.viewmodel.GenEditingViewModel;
 import dev.ikm.tinkar.common.id.PublicId;
 import dev.ikm.tinkar.common.id.PublicIds;
 import dev.ikm.tinkar.coordinate.stamp.calculator.Latest;
@@ -19,10 +28,12 @@ import dev.ikm.tinkar.coordinate.stamp.calculator.StampCalculator;
 import dev.ikm.tinkar.entity.FieldRecord;
 import dev.ikm.tinkar.entity.PatternEntityVersion;
 import dev.ikm.tinkar.entity.SemanticEntityVersion;
+import dev.ikm.tinkar.terms.EntityFacade;
 import dev.ikm.tinkar.terms.TinkarTerm;
 import javafx.scene.Node;
 import javafx.scene.control.Separator;
 import javafx.scene.layout.Pane;
+import org.carlfx.cognitive.loader.InjectViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +41,9 @@ import java.util.UUID;
 import java.util.function.Consumer;
 
 public class KlFieldHelper {
+
+    @InjectViewModel
+    private static GenEditingViewModel genEditingViewModel;
 
     private static Separator createSeparator() {
         Separator separator = new Separator();
@@ -151,8 +165,20 @@ public class KlFieldHelper {
                     readOnlyNode = klFloatFieldFactory.create(observableField, viewProperties.nodeView(), false).klWidget();
                 } else if (dataTypeNid == TinkarTerm.INTEGER_FIELD.nid()) {
                     KlIntegerFieldFactory klIntegerFieldFactory = new KlIntegerFieldFactory();
-                    readOnlyNode = klIntegerFieldFactory.create(observableField, viewProperties.nodeView(), false).klWidget();
-                } else if (dataTypeNid == TinkarTerm.BOOLEAN_FIELD.nid()) {
+                    KLReadOnlyDataTypeControl<Integer> readOnlyNode2 = klIntegerFieldFactory.create(observableField, viewProperties.nodeView(), false).klWidget();
+                    readOnlyNode2.setOnEditAction(()-> {
+                        System.out.println("entered setOnEditAction()");
+//                        EntityFacade semantic = genEditingViewModel.getPropertyValue(SEMANTIC);
+//                        // notify bump out to display edit fields in bump out area.
+//                        EvtBusFactory.getDefaultEvtBus()
+//                                .publish(genEditingViewModel.getPropertyValue(WINDOW_TOPIC),
+//                                        new PropertyPanelEvent(actionEvent.getSource(),
+//                                                SHOW_EDIT_SEMANTIC_FIELDS, semantic));
+//                        // open properties bump out.
+//                        EvtBusFactory.getDefaultEvtBus().publish(genEditingViewModel.getPropertyValue(WINDOW_TOPIC), new PropertyPanelEvent(actionEvent.getSource(), OPEN_PANEL));
+                    });
+                    readOnlyNode = readOnlyNode2;
+                }  else if (dataTypeNid == TinkarTerm.BOOLEAN_FIELD.nid()) {
                     KlBooleanFieldFactory klBooleanFieldFactory = new KlBooleanFieldFactory();
                     readOnlyNode = klBooleanFieldFactory.create(observableField, viewProperties.nodeView(), false).klWidget();
                 }
